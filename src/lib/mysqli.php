f508df1c05cfd5eb293eef90278fd2022838b009
mariko_mr <ma.kakiuchi@gmail.com>
2023-10-07 20:34:51 +0900

    バリデーション処理を追加 #1

--- src/lib/mysqli.php
+++ src/lib/mysqli.php
@@ -1,27 +1,33 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../log_analysis.php';
 
 function dbConnect()
 {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-    $dotenv->load();
-    $dbDatabase = $_ENV['DB_DATABASE'];
-    $dbUserName = $_ENV['DB_USERNAME'];
-    $dbPassword = $_ENV['DB_PASSWORD'];
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+  $dotenv->load();
+  $dbDatabase = $_ENV['DB_DATABASE'];
+  $dbUserName = $_ENV['DB_USERNAME'];
+  $dbPassword = $_ENV['DB_PASSWORD'];
 
-    try {
-        $dbh = new PDO($dbDatabase, $dbUserName, $dbPassword);
-    } catch (PDOException $e) {
-        echo '接続失敗: ' . $e->getMessage() . PHP_EOL;
-        exit();
-    }
-    return $dbh;
+  try {
+    $dbh = new PDO($dbDatabase, $dbUserName, $dbPassword);
+  } catch (PDOException $e) {
+    echo '接続失敗: ' . $e->getMessage() . PHP_EOL;
+    exit();
+  }
+  return $dbh;
 }
 
+/**
+ * ここを修正
+ *
+ * バリデーション処理を追加
+ */
 function getTopArticles($dbh)
 {
-    $sql = <<< EOT
+  $sql = <<< EOT
     SELECT
       domain_code, page_title, count_views
     FROM
@@ -32,32 +38,39 @@ function getTopArticles($dbh)
       :limit;
 EOT;
 
-    echo  PHP_EOL .
-        '最もビュー数の多い記事を表示します' . PHP_EOL .
-        '記事数を整数で指定してください: ';
-    $limit = fgets(STDIN);
+  echo  PHP_EOL .
+    '最もビュー数の多い記事を表示します' . PHP_EOL .
+    '記事数を１以上の整数で指定してください: ';
+  $validatedLimit = validateLimit(trim(fgets(STDIN)));
 
-    // クエリの実行
-    try {
-        $sth = $dbh->prepare($sql);
-        $sth->bindParam(':limit', $limit, PDO::PARAM_INT);
-        $sth->execute();
-    } catch (PDOException $e) {
-        echo '取得失敗: ' . $e->getMessage() . PHP_EOL;
-        exit();
-    }
+  // クエリの実行
+  try {
+    $sth = $dbh->prepare($sql);
+    $sth->bindParam(':limit', $validatedLimit, PDO::PARAM_INT);
+    $sth->execute();
+  } catch (PDOException $e) {
+    echo '取得失敗: ' . $e->getMessage() . PHP_EOL;
+    exit();
+  }
 
-    // 最もビュー数の多い記事を表示する
-    while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
-        echo '”' . $row['domain_code'] . '”, ' .
-            '”' .  $row['page_title'] . '”, ' .
-            $row['count_views'] . PHP_EOL;
-    }
+  // 最もビュー数の多い記事を表示する
+  while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+    echo '”' . $row['domain_code'] . '”, ' .
+      '”' .  $row['page_title'] . '”, ' .
+      $row['count_views'] . PHP_EOL;
+  }
+
+  return selectTask();
 }
 
+/**
+ * ここを修正
+ *
+ * バリデーション処理を追加
+ */
 function getDomainViews($dbh)
 {
-    $sql = <<< EOT
+  $sql = <<< EOT
     SELECT
       domain_code, count_views
     FROM
@@ -70,39 +83,44 @@ function getDomainViews($dbh)
       1;
 EOT;
 
-    echo  PHP_EOL .
-        '指定したドメインコードに対して、人気順にソートし、ドメインコード名と合計ビュー数を提示します' . PHP_EOL .
-        'ドメインコードを指定してください(例: en de): ';
+  echo  PHP_EOL .
+    '指定したドメインコードに対して、人気順にソートし、ドメインコード名と合計ビュー数を提示します' . PHP_EOL;
+
+  // ドメインごとの合計ビュー数を格納する連想配列('en' => 10001)
+  $domainViews = [];
+
+  while (empty($domainViews)) {
+    echo 'ドメインコードを指定してください(例: en de): ';
 
     // ドメインコードを分割し、配列に格納
     $domainCodes = explode(' ', trim(fgets(STDIN)));
 
-    // ドメインごとの合計ビュー数を格納する連想配列('en' => 10001)
-    $domainViews = [];
-
     // クエリの実行
     try {
-        // 各ドメインコードごとにデータを取得
-        foreach ($domainCodes as $domainCode) {
-            $sth = $dbh->prepare($sql);
-            $sth->bindParam(':domain_code', $domainCode, PDO::PARAM_STR);
-            $sth->execute();
+      // 各ドメインコードごとにデータを取得
+      foreach ($domainCodes as $domainCode) {
+        $sth = $dbh->prepare($sql);
+        $sth->bindParam(':domain_code', $domainCode, PDO::PARAM_STR);
+        $sth->execute();
 
-            // クエリの結果を取得
-            $result = $sth->fetch(PDO::FETCH_ASSOC);
+        // クエリの結果を取得
+        $result = $sth->fetch(PDO::FETCH_ASSOC);
 
-            // ドメインコードが存在する場合、連想配列に格納
-            if ($result) {
-                $domainViews[$result['domain_code']] = $result['count_views'];
-            }
+        // ドメインコードが存在する場合、連想配列に格納
+        if ($result) {
+          $domainViews[$result['domain_code']] = $result['count_views'];
         }
+      }
     } catch (PDOException $e) {
-        echo '取得失敗: ' . $e->getMessage() . PHP_EOL;
-        exit();
+      echo '取得失敗: ' . $e->getMessage() . PHP_EOL;
+      exit();
     }
+  }
 
-    // ドメインコードと合計ビューを表示
-    foreach ($domainViews as $code => $views) {
-        echo '”' . $code . '”, ' . $views . PHP_EOL;
-    }
+  // ドメインコードと合計ビューを表示
+  foreach ($domainViews as $code => $views) {
+    echo '”' . $code . '”, ' . $views . PHP_EOL;
+  }
+
+  return selectTask();
 }
--- src/log_analysis.php
+++ src/log_analysis.php
@@ -2,28 +2,79 @@
 
 require_once __DIR__ . '/lib/mysqli.php';
 
-// データベースに接続する
 $dbh = dbConnect();
+$validatedStdin = selectTask();
+startAnalyze($validatedStdin, $dbh);
 
 /**
  * ここを修正
- * 「9: 終了する」を追加
+ * バリデーション処理を追加
+ *
  */
-// タスクを選択
-echo 'Wikipediaのアクセスログを分析します。' . PHP_EOL . PHP_EOL .
-    '1: 最もビュー数の多い記事を表示' . PHP_EOL .
-    '2: 人気記事の合計ビュー数を表示' . PHP_EOL .
-    '9: 終了する' . PHP_EOL . PHP_EOL .
-    '上記から選択してください(1, 2, 9を選択): ';
-$stdin = trim(fgets(STDIN));
+/**
+ * タスクを選択する
+ *
+ * @return string $stdin
+ */
+function selectTask(): string
+{
+    echo  PHP_EOL .
+        'Wikipediaのアクセスログを分析します。' . PHP_EOL . PHP_EOL .
+        '1: 最もビュー数の多い記事を表示' . PHP_EOL .
+        '2: 人気記事の合計ビュー数を表示' . PHP_EOL .
+        '9: 終了する' . PHP_EOL . PHP_EOL .
+        '上記から選択してください(1, 2, 9を選択): ';
+    $stdin = trim(fgets(STDIN));
 
-// TODO: 1, 2を選択している間はループするようにする
-// TODO: バリデーション処理をする
-if ($stdin === '1') {
-    // 最もビュー数の多い記事を表示
-    getTopArticles($dbh);
-} elseif ($stdin === '2') {
-    // 人気記事の合計ビュー数を表示
-    getDomainViews($dbh);
+    return validateStdin($stdin);
 }
-exit();
+
+/**
+ * ここを修正
+ *
+ * 1, 2を選択している間はループするようにする
+ */
+function startAnalyze($validatedStdin, $dbh)
+{
+    while ($validatedStdin !== '9') {
+        if ($validatedStdin === '1') {
+            $validatedStdin = getTopArticles($dbh);
+        } elseif ($validatedStdin === '2') {
+            $validatedStdin = getDomainViews($dbh);
+        }
+    }
+
+    if ($validatedStdin === '9') {
+        echo PHP_EOL . 'プログラムを終了します';
+        exit();
+    }
+}
+
+/**
+ * ここを追加
+ */
+function validateStdin($stdin)
+{
+    $allowedNumbers  = ['1', '2', '9'];
+
+    // 入力値のバリデーション処理
+    while (!(in_array($stdin, $allowedNumbers))) {
+        echo '【1, 2, 9】のどれかを入力してください: ';
+        $stdin = trim(fgets(STDIN));
+    }
+    return $stdin;
+}
+
+/**
+ * ここを追加
+ */
+function validateLimit($stdin)
+{
+    // バリデーション処理を追加する
+    while (!(preg_match('/\A[1-9][0-9]*\z/', $stdin))) {
+        echo '記事数を１以上の整数で指定してください: ';
+        $stdin = trim(fgets(STDIN));
+    }
+
+    return $stdin;
+}
